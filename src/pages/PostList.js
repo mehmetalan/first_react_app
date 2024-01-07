dd5b9e5b45c3ea1bbc53c1ce237a8dfc950179ac
Mehmet ALAN <mehmet25alan@gmail.com>
2024-01-07 14:25:17 +0300

    Code review.

--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -1,58 +1,52 @@
 import axios from "axios";
 import React from "react";
-import { Table }  from 'react-bootstrap'; 
-import Button from 'react-bootstrap/Button';
-import { Link, Route, Routes } from 'react-router-dom';
-import '../index.css';
+import { Table } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { Link, Route, Routes } from "react-router-dom";
+import "../index.css";
 
 const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 export function PostList() {
-    let listItems
-    const [posts, setPost] = React.useState(null);
+  let listItems;
+  const [posts, setPost] = React.useState(null);
 
-    React.useEffect(() => {
-        axios.get(baseURL).then((response) => {
-        setPost(response.data);
-        listItems = response.data.map(record => (
-            <tr>
-              <td>{record.title}</td>
-              <td>{record.body}</td>
-            </tr>
-          ))
-        });
-    }, []);
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
 
-    if (!posts) return <></>;
+  if (!posts) return <></>;
 
-    return (
-        <div className="allPosts">
-          <h2 className="tableheader">All Posts</h2>
-          <Table striped bordered hover className="custom-table"> 
-          <thead> 
-                    <tr> 
-                        <th>Title</th> 
-                        <th>Text</th> 
-                        <th>Action</th> 
-                    </tr> 
-                </thead>
-                <tbody>
+  return (
+    <div className="allPosts">
+      <h2 className="tableheader">All Posts</h2>
+      <Table striped bordered hover className="custom-table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Text</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
           {posts.map((post) => {
-             return (
-                <>
-                   <tr>
-                    <th>{post.title}</th>
-                    <th>{post.body}</th>
-                    <Link to={'/post/' + post.id}>
-                      <Button bsStyle="primary">Detail</Button>
-                    </Link>
-                   </tr>
-                </>
-             );
+            return (
+              <>
+                <tr>
+                  <th>{post.title}</th>
+                  <th>{post.body}</th>
+                  <Link to={"/post/" + post.id}>
+                    <Button bsStyle="primary">Detail</Button>
+                  </Link>
+                </tr>
+              </>
+            );
           })}
-          </tbody>
-          </Table> 
-          <Button>AA</Button>
-        </div>
-      );
+        </tbody>
+      </Table>
+      <Button>AA</Button>
+    </div>
+  );
 }
