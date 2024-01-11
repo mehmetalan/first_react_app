cdb77d49ad1e45f7067a5541662c97f0804cadbe
Mehmet Alan <malan@isbak.istanbul>
2024-01-11 11:40:37 +0300

    AddPost inputları eklendi.

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -1,35 +1,63 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 export default function AddPost() {
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get(`${baseURL}/1`).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
+  const [post, setPost] = useState({});
+  const [titleInput, setTitleInput] = useState("");
+  const [bodyInput, setBodyInput] = useState("");
 
   function createPost() {
     axios
       .post(baseURL, {
-        title: "Hello World!",
-        body: "This is a new post.",
+        title: titleInput,
+        body: bodyInput,
       })
       .then((response) => {
         setPost(response.data);
+        setTitleInput("");
+        setBodyInput("");
       });
   }
 
-  if (!post) return "No post!";
+  const showPost = post.title && post.body;
 
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      {showPost ? (
+        <>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </>
+      ) : (
+        <h3>New Post</h3>
+      )}
+      <div className="row text-center">
+        <div className="col-md-6 mx-auto">
+          <label>Title: </label>
+          <Form.Control
+            type="text"
+            value={titleInput}
+            onChange={(e) => setTitleInput(e.target.value)}
+            placeholder="Title"
+          />
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-6 mx-auto">
+          <label>Body: </label>
+          <Form.Control
+            type="text"
+            value={bodyInput}
+            onChange={(e) => setBodyInput(e.target.value)}
+            placeholder="Body"
+          />
+        </div>
+      </div>
+      <br></br>
       <Button onClick={createPost}>Create Post</Button>
     </div>
   );
