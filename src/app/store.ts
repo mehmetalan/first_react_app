d2ce1f6c33eb8b98418c9c02a3f8c5c5f590fe02
Mehmet Alan <malan@isbak.istanbul>
2024-01-30 16:22:47 +0300

    redux-toolkit eklendi. interface tanımlandı.

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import usersReducer from "../features/users/usersSlice";
+import postsReducer from "../features/posts/postsSlice";
+import commentsReducer from "../features/comments/commentsReducer";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    posts: postsReducer,
+    comments: commentsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
---
+++ src/features/users/usersSlice.ts
@@ -0,0 +1,60 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+import { userType } from "../../assets/interfaces";
+
+interface usersStateType {
+  users: userType[] | null;
+  error: string;
+  loading: boolean;
+}
+
+const initialState: usersStateType = {
+  users: null,
+  error: "",
+  loading: true,
+};
+
+export const fetchUsers = createAsyncThunk(
+  "users/fetch-all",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const users = response.data;
+
+      return users;
+    } catch (error: any) {
+      const message = error?.message || "Error fetching the users";
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    reset: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.error = "";
+        state.loading = true;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.error = "";
+        state.loading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
+  },
+});
+
+export const { reset } = usersSlice.actions;
+export default usersSlice.reducer;
